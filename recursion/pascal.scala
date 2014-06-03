bace9e8dd052fd3ff1648e00b4df7108ae68fb0f
WestleyArgentum <WestleyArgentum@gmail.com>
2014-06-03 11:28:40 -0700

    fix whitespace

--- recursion/pascal.scala
+++ recursion/pascal.scala
@@ -1,8 +1,8 @@
 
 def pascal(c: Int, r: Int): Int = {
-	if (c == 0 || c == r) {
-	  return 1
-	}
+  if (c == 0 || c == r) {
+    return 1
+  }
 
-	pascal(c - 1, r - 1) + pascal(c, r - 1)
+  pascal(c - 1, r - 1) + pascal(c, r - 1)
 }
